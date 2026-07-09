80f187e520193bdcb33e07861cf7a19a03b5af2f
Jhan-Jhan17 <24-03424@g.batstate-u.edu.ph>
2026-07-09 12:16:14 +0800

    bypass

--- src/main/java/com/linhs/portal/config/WebConfig.java
+++ src/main/java/com/linhs/portal/config/WebConfig.java
@@ -4,21 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        
-        // This gets the absolute physical path to your "uploads" folder on your computer
-        Path uploadDir = Paths.get("uploads");
-        String uploadPath = uploadDir.toFile().getAbsolutePath();
-
-        // This creates the bridge: Any web request starting with /uploads/ will be allowed to read from your physical folder!
+        // The "file:uploads/" path works perfectly inside Docker environments
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:/" + uploadPath + "/");
+                .addResourceLocations("file:uploads/");
     }
 }
